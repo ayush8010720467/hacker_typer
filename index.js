81c29f376f47e7da93a5577031ea6256b6d20df0
Ayush Katiyar <ayushkatiyar096@gmail.com>
2021-09-19 17:40:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function test(){
             return function(){
                 let code = document.getElementById('source');
                 if(currentPos-3<data.length){
-                    code.innerHTML = data.substring(0,currentPos+3).replace(/[\u00A0-\u9999<>\&]/gim, function(a) {
-                        return "&#" + a.charCodeAt(0) + ";"
-                    });
+                    code.innerHTML = data.substring(0,currentPos+3);
                     currentPos +=3;
                 } else{
                     code.innerHTML = data.substring(0);
